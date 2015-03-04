564d7a7820c7e09afd2f4b871c26ef7d1c30191a
卓戈 <zhuoge.zhb@alibaba-inc.com>
2015-03-04 15:04:20 +0800

    prepare to test

--- api.js
+++ api.js
@@ -12,24 +12,15 @@ router.get("/", function(req, res) {
 });
 
 /*
- * 注册用户:
- *
- * 若姓名和电话号码对已存在，则覆盖，否则创建新记录
- */
-router.post("/register", function(req, res) {
-
-});
-
-/*
  * 查询用户加入的所有组的信息
  *
  */
 router.get("/listGroup", function(req, res) {
-    // 用户ID
     var uid = req.query.uid;
-    var sql = "SELECT * FROM usergroup WHERE uid = '" + uid + "'";
+    var sql = "SELECT * FROM `group` AS g WHERE EXISTS (" +
+        "SELECT * FROM `usergroup` AS ug WHERE ug.uid='" +
+        uid + "' AND ug.gid = g.id)";
     db.query(sql, function(err, rows, fields) {
-        // 直接输出结果
         res.json(rows);
     });
 });
@@ -39,15 +30,62 @@ router.get("/listGroup", function(req, res) {
  *
  */
 router.get("/listUser", function(req, res) {
+    var gid = req.query.gid;
+    var sql = "SELECT * FROM `user` AS u WHERE EXISTS (" +
+        "SELECT * FROM `usergroup` AS ug WHERE ug.gid='" +
+        gid + "' AND ug.uid = u.id)";
+    db.query(sql, function(err, rows, fields) {
+        res.json(rows);
+    });
+});
 
+/*
+ * 注册用户:
+ *
+ * 若姓名和电话号码对已存在，则覆盖，否则创建新记录
+ *
+ */
+router.post("/register", function(req, res) {
+    var name = req.body.name;
+    var phone = req.body.phone;
+    // 先检查一下是否存在
+    var sql = "SELECT * FROM `user` WHERE name = '" + name +
+        "' AND phone = '" + phone + "'";
+    db.query(sql, function(err, rows, fields) {
+        // 已存在
+        if (rows.length === 1) {
+            res.json({id: rows[0].id});
+        } else {
+            sql = "INSERT INTO `user` (`name`, `phone`) VALUES ('" +
+                name + "', '" + phone + "')";
+            db.query(sql, function(err, result) {
+                res.json({id: result.insertId});
+            });
+        }
+    });
 });
 
 /*
  * 更新个人信息
  *
+ * 为了简单起见, 使用全量更新(除名称外)
+ *
  */
-router.get("/update", function(req, res) {
+router.post("/update", function(req, res) {
+    var phone = req.body.phone;
+    var uid = req.body.uid;
+    var name = req.body.name;
 
+    var sql = "UPDATE `user` SET phone = '" + phone + "' WHERE id = '" +
+        uid + "' AND name = '" + name + "'";
+
+    db.query(sql, function(err, result) {
+        if (result.affectedRows === 1) {
+            res.json({status: 0});
+        } else {
+            res.json({status: -1});
+        }
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+// BAE约定的端口号
 var port = 18080;
 
 var express = require("express");
 var app = express();
+
+// POST参数获取, 被注释的那几行暂时用不上
+var bodyParser = require("body-parser");
+// var multer = require("multer");
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(multer());
 
 // 静态文件
 app.use(express.static("static"));
