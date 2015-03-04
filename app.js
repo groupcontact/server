f6a48c872db7277df0847040077587ba02655f74
卓戈 <zhuoge.zhb@alibaba-inc.com>
2015-03-04 17:01:05 +0800

    add jade template engine

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var port = 18080;
 var express = require("express");
 var app = express();
 
+// 使用jade模板引擎
+app.set('views', './views');
+app.set('view engine', 'jade');
+
 // POST参数获取, 被注释的那几行暂时用不上
 var bodyParser = require("body-parser");
 // var multer = require("multer");
--- home.js
+++ home.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-
+router.get("/", function(req, res) {
+    res.render('index', {title: "群通讯录"});
+});
 
 module.exports = router;
