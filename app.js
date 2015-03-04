0a65de84f2baf40cba25a03d85b2980762cb8af6
卓戈 <zhuoge.zhb@alibaba-inc.com>
2015-03-04 11:51:54 +0800

    use expressjs to build helloworld

---
+++ app.js
@@ -0,0 +1,10 @@
+var port = 18080;
+
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello World");
+});
+
+var server = app.listen(port);
