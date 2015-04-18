fd40f8af1051d0d680b9400f40fc722ca72786af
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-04-18 16:55:59 +0800

    try debug

--- controllers/api/v2/users/index.js
+++ controllers/api/v2/users/index.js
@@ -28,6 +28,54 @@ function GeneralCallback(res, successFunc, failFunc) {
     };
 }
 
+// 用户信息
+router.get("/:id", function(req, res) {
+    var uid = req.param("id");
+    var name = req.query.name;
+});
+
+// 更新用户信息
+router.put("/:id", function(req, res) {
+    var uid = req.params.id;
+    var phone = req.body.phone;
+    var name = req.body.name;
+    var ext = req.body.ext;
+    var password = req.body.password;
+
+    // 实际调用更新操作
+    var update = function() {
+        user.update(uid, name, phone, ext, new GeneralCallback(res, function(result) {
+            res.json({status: 1});
+        }, "设置用户信息失败").callback);
+    };
+
+    // 权限验证
+    user.auth(uid, password, new GeneralCallback(res, function(rows) {
+        update();
+    }, "无权限").callback);
+});
+
+// 添加用户
+router.post("/", function(req, res) {
+    var phone = req.body.phone;
+    var password = req.body.password;
+
+    user.exist(phone, new GeneralCallback(res, function(rows) {
+            var shasum = crypto.createHash('sha1');
+            shasum.update(password);
+            if (shasum.digest("hex") === rows[0].password) {
+                res.json({status: 2, id: rows[0].id});
+            } else {
+                res.json({status: -1, info: "密码错误"});
+            }
+        }, function() {
+            user.create(phone, password, new GeneralCallback(res, function(result) {
+                    res.json({status: 1, id: result.insertId});
+                }, "创建用户失败").callback);
+        }).callback);
+});
+
+
 // 朋友列表
 router.get("/:id/friends", function(req, res) {
 
@@ -56,52 +104,6 @@ router.post("/:id/groups", function(req, res) {
 // 退出群组
 router.delete("/:id/groups", function(req, res) {
 
-});
-
-// 用户信息
-router.get("/:id", function(req, res) {
-    var uid = req.param("id");
-    var name = req.query.name;
-});
-
-// 更新用户信息
-router.put("/:id", function(req, res) {
-    var uid = req.params.id;
-    var phone = req.body.phone;
-    var name = req.body.name;
-    var ext = req.body.ext;
-    var password = req.body.password;
-
-    // 实际调用更新操作
-    var update = function() {
-        user.update(uid, name, phone, ext, new GeneralCallback(res, null,
-            "设置用户信息失败").callback);
-    };
-
-    // 权限验证
-    user.auth(uid, password, new GeneralCallback(res, function(rows) {
-        update();
-    }, "无权限").callback);
-});
-
-// 添加用户
-router.post("/", function(req, res) {
-    var phone = req.body.phone;
-    var password = req.body.password;
-
-    user.exist(phone, new GeneralCallback(res, function(rows) {
-            var shasum = crypto.createHash('sha1');
-            shasum.update(password);
-            if (shasum.digest("hex") === rows[0].password) {
-                res.json({status: 2, id: rows[0].id});
-            } else {
-                res.json({status: -1, info: "密码错误"});
-            }
-        }, function() {
-            user.create(phone, password, new GeneralCallback(res, function(result) {
-                    res.json({status: 1, id: result.insertId});
-                }, "创建用户失败").callback);
-        }).callback);
 });
 
 module.exports = router;
