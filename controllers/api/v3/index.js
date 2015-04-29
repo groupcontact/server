3ace40ff062be734e5c64b1db4dd65099723f178
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-04-30 01:08:46 +0800

    update order

--- controllers/api/v3/index.js
+++ controllers/api/v3/index.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
+router.all("/*", function(req, res, next) {
+    // if not secure
+    if (!req.secure) {
+        res.json({ status: -1, info: "请使用https协议访问API" });
+        return;
+    }
+    next();
+});
+
 var users = require("./users");
 var groups = require("./groups");
 var test = require("./test");
@@ -8,14 +17,5 @@ var test = require("./test");
 router.use("/users", users);
 router.use("/groups", groups);
 router.use("/test", test);
-
-router.all("/*", function(req, res, next) {
-    // if not secure
-    if (!req.secure) {
-        res.json({ status: -1, info: "请使用Https协议访问API" });
-        return;
-    }
-    next();
-});
 
 module.exports = router;
